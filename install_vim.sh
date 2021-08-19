1c3574164533f7c6e52483b2a7f97ef6bc957314
Juil Koo <63days@kaist.ac.kr>
2021-08-20 00:05:32 +0900

    Update install_vim.sh

--- install_vim.sh
+++ install_vim.sh
@@ -9,7 +9,7 @@ sudo apt-get install -y nodejs
 
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn<Paste>
+sudo apt-get update && sudo apt-get install yarn
 
 # Install vim plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
