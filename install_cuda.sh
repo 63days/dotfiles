cdae4f35b4b2294944fd06c59a1239277b1f5fb3
Juil Koo <63days@kaist.ac.kr>
2021-08-20 01:02:19 +0900

    Update install_cuda.sh

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,9 +1,9 @@
-# cuda-11.2
-wget https://developer.download.nvidia.com/compute/cuda/11.2.0/local_installers/cuda_11.2.0_460.27.04_linux.run
-sudo sh cuda_11.2.0_460.27.04_linux.run
+# cuda-11.0
+wget http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda_11.0.2_450.51.05_linux.run
+sudo sh cuda_11.0.2_450.51.05_linux.run
 
-echo 'export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda-11.2/lib64' >> ~/.zshrc
-echo 'export PATH=${PATH}:/usr/local/cuda-11.2/bin' >> ~/.zshrc
+echo 'export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda-11.0/lib64' >> ~/.zshrc
+echo 'export PATH=${PATH}:/usr/local/cuda-11.0/bin' >> ~/.zshrc
 source ~/.zshrc
 
     
