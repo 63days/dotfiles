d22c166de2c7c8a7fd7c1eeb12f97c579126ec4b
juil <63days@kaist.ac.kr>
2021-08-27 07:06:17 +0000

    remove redundant files that were included Dockerfile

--- install_cuda.sh
+++
@@ -1,9 +0,0 @@
-# cuda-11.0
-wget http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda_11.0.2_450.51.05_linux.run
-sudo sh cuda_11.0.2_450.51.05_linux.run
-
-echo 'export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda-11.0/lib64' >> ~/.zshrc
-echo 'export PATH=${PATH}:/usr/local/cuda-11.0/bin' >> ~/.zshrc
-source ~/.zshrc
-# cudnn version: libcudnn8_8.0.5.39-1+cuda11.0_amd64.deb
-    
