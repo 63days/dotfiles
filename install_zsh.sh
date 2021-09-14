cd6c7cb5151c47769d78daa5dcba3f0e420831f5
JuilKoo <63day@kaist.ac.kr>
2021-09-14 13:29:26 +0900

    update install vim and zsh

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 # oh-my-zsh
+
+function echo_fail {
+  # echo first argument in red
+  printf "\e[31m✘ ${1}"
+  # reset colours back to normal
+  printf "\033\e[0m"
+  echo ""
+}
+
+# display a message in green with a tick by it
+# example
+# echo echo_fail "Yes"
+function echo_pass {
+  # echo first argument in green
+  printf "\e[32m✔ ${1}"
+  # reset colours back to normal
+  printf "\033\e[0m"
+  echo ""
+}
+
+function program_is_installed {
+    # set to 1 initially
+    local return_=1
+    # set to 0 if not found
+    type $1 >/dev/null 2>&1 || { local return_=0; }
+    # return value
+    echo "$return_"
+}
+
+if [ $(program_is_installed zsh) == 1 ]; then
+    echo_pass zsh
+else
+    echo_fail zsh
+    sudo apt-get install -y zsh
+fi
+
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # auto-suggestions
@@ -18,7 +54,8 @@ echo 'prompt_context() {
 	fi
 }' >> ${HOME}/.zshrc
 
-if [ $(program_is_installed nvim) == 1]; then
+if [ $(program_is_installed nvim) == 1 ]; then
+	
 	echo 'alias vi="nvim"' >> ~/.zshrc
 	echo 'alias vim="nvim"' >> ~/.zshrc
 fi
