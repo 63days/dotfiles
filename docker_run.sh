6c96acca5d9bd132b58f5d8289ffa97a1d178d75
Juil Koo <63days@kaist.ac.kr>
2022-07-19 10:16:40 +0900

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,11 +1,14 @@
-docker run -it \
-    --ipc=host \
-    --pid=host \
+docker run \
+    -it \
+    --ipc=host \ # to prevent memory errors
+    --cap-add LINUX_IMMUTABLE \ # to enable chattr
+    --volume /tmp/.X11-unix:/tmp/.X11-unix:ro \ # for X11
+    -e DISPLAY=unix$DISPLAY \ # for X11
     --restart unless-stopped \
     --gpus all \
-    -p @PORT_NUM:22 \
+    #--gpus '"device=0,1"' # You can assign specific gpu devices.
+    -p @PORT_NUM:22 \ # port forwarding
     --name @CONTAINER_NAME \
-    -h geometry \
-    -v $HOME/docker_home:$HOME \
-    @IMAGE_NAME \
+    -v @HOST_DIR:@CONTAINER_DIR \ # e.g. -v /home/juil/docker_home:/home/juil
+    @IMAGE_NAME \ # docker image
     /bin/zsh
