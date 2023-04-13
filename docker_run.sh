7294a4c05d1d11bd36af29059f33a4f7d23afc07
Juil Koo <63days@kaist.ac.kr>
2023-04-13 15:06:13 +0900

    run mitsuba in docker

--- docker_run.sh
+++ docker_run.sh
@@ -1,17 +1,21 @@
+#!/bin/bash
+
 docker run \
     -it \
     --ipc=host \
     --cap-add LINUX_IMMUTABLE \
-    --volume /tmp/.X11-unix:/tmp/.X11-unix:ro \
-    -p 6364:22 \
+    -p 6363:22 \
     -e DISPLAY=$DISPLAY \
     --restart unless-stopped \
     --pid=host \
-    --gpus \
-    'all,"capabilities=compute,utility,graphics,display"'\
-    --name juil-temp \
-    -v /home/juil/docker_home:/home/juil \
+    --gpus 'all,"capabilities=compute,utility,graphics,display"'\
+    --hostname docker1 \
+    --name juil \
     -v="$HOME/.Xauthority:/root/.Xauthority:rw" \
-    -v="/home/shared:/shared" \
-    juil_temp \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+    -v /home/juil/docker_home:/home/juil \
+    -v "/scratch:/scratch" \
+    -v "/home/shared:/home/shared" \
+		-v "/home:/home/blackhole:ro" \
+    juil:1.0 \
     /bin/zsh
